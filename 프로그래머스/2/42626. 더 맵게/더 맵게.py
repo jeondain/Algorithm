b21c1ff62375d7990dec821479514f5328d155b9
jeondain <120189161+jeondain@users.noreply.github.com>
2024-09-27 20:41:00 +0900

    [level 2] Title: 더 맵게, Time: 1643.30 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -4,12 +4,10 @@ input = sys.stdin.readline
 
 def solution(scoville, K):
     count = 0
-    q = []
-    for i in scoville:
-        heapq.heappush(q, i)
-    while q[0] < K:
-        if(len(q) < 2):        
+    heapq.heapify(scoville)
+    while scoville[0] < K:
+        if(len(scoville) < 2):        
             return -1
-        heapq.heappush(q, (heapq.heappop(q) + heapq.heappop(q)*2))
+        heapq.heappush(scoville, (heapq.heappop(scoville) + heapq.heappop(scoville)*2))
         count += 1
     return count

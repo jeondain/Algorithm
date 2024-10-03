7c81b8488cf0250e8c93ea495ae32068e82a1c0d
jeondain <120189161+jeondain@users.noreply.github.com>
2024-10-03 23:29:59 +0900

    [Silver IV] Title: 카드, Time: 100 ms, Memory: 42900 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.py
+++ 백준/Silver/11652. 카드/카드.py
@@ -2,18 +2,16 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-card = []
-for _ in range(n):
-    card.append(int(input()))
 
 # 카드 등장 횟수 저장
 hash = {}
-for num in card:
-    if num not in hash:
-        hash[num] = 1
+for _ in range(n):
+    card = int(input())
+    if card not in hash:
+        hash[card] = 1
     else:
-        hash[num] += 1
+        hash[card] += 1
 
 max_count = max(hash.values())
 result = min([key for key, value in hash.items() if value == max_count])   
-print(result)        
+print(result)   
